bb03286f8096a006d794df92ff49213a52c7678f
WNbelex <zjm2108587704@163.com>
2025-09-13 16:54:35 +0800

    Add files via upload

---
+++ BUUreverse3/v4.py
@@ -0,0 +1,14 @@
+#用于动态代码
+
+text = "e3nifIH9b_C@n@dH"
+ascii_list = [ord(c) for c in text]
+print(ascii_list)
+for i in range(0, len(ascii_list)):
+    ascii_list[i] = ascii_list[i] - i
+print(ascii_list)
+txt=[chr(c) for c in ascii_list]
+print(txt)
+word=""
+for c in txt:
+    word += c
+print(word)
